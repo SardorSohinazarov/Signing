b2fca055b2cd83779f89aa12a5aa276119e0b454
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-25 05:28:17 +0500

    Add project files.

---
+++ SigningAPI/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SigningAPI.Entities;
+
+namespace SigningAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDTO loginDTO)
+        {
+            return Ok("Loginned successfully");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDTO registerDTO)
+        {
+            return Ok("Registred successfully");
+        }
+    }
+}
