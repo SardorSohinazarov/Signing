e525e4798574ac60b3ad3a05e59da009e9b98be3
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-28 23:18:08 +0500

    DTOs and Database Connection Implemented

---
+++ SigningAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SigningAPI.Entities;
+
+namespace SigningAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ SigningAPI/Models/RegisterDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace SigningAPI.Models
+{
+    public class RegisterDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+
+        public ICollection<int>? Roles { get; set; }
+    }
+}
